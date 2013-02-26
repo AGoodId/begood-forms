af5cf8a78cb20315f8a7d2e6c363b23f213cb79f
Olof Sjöbergh <olofsj@gmail.com>
2013-02-26 10:47:24 +0100

    Add initial structure.

---
+++ README.markdown
@@ -0,0 +1,9 @@
+# begood-forms
+
+begood-forms is a Django app that let's the editor easily creates forms
+such as contact forms or forms that post to other sites.
+
+## Installation
+
+Clone the [Git repository](https://github.com/agoodid/begood-forms).
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="begood-forms",
+    version="0.1.0",
+    description="",
+    author="AGoodId",
+    author_email="teknik@agoodid.com",
+    maintainer="AGoodId",
+    maintainer_email="teknik@agoodid.se",
+    url="https://github.com/agoodid/begood-forms",
+    license="MIT",
+    packages=[
+        "begood_forms",
+        "begood_sites.templatetags",
+    ],
+    long_description=read("README.markdown"),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
