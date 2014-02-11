d139c44a13d672f39f810e9a3a9e38a1035310ac
Olof Sjöbergh <olofsj@gmail.com>
2014-02-11 10:49:41 +0100

    Add anchor tags to scroll to the correct place when submitting the form.

--- begood_forms/templatetags/begood_forms_tags.py
+++ begood_forms/templatetags/begood_forms_tags.py
@@ -39,6 +39,7 @@ def render_form(form, template_path='begood_forms/form.html'):
     t = loader.get_template(template_path)
     action = form.target if form.action == 'ex' else ''
     return t.render(RequestContext(form.request, {
+      'begood_form': form,
       'form': form.form,
       'description': form.description,
       'valid_content': form.valid_content,
